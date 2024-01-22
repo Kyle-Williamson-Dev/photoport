c49046f89530d25f414dcd531607ef6a160c6e30
Kyle Williamson <kwilliamson0232@gmail.com>
2024-01-22 13:22:33 -0700

    still trying

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -2,7 +2,7 @@ class PortfolioController < ApplicationController
 
   def index
     @sections = [
-      { title: "Portraits", path: portraits_index_path, image_url: view_context.asset_path("/assets/Jaclyn2.jpg") },
+      { title: "Portraits", path: portraits_index_path, image_url: view_context.asset_path("/assets/images/Jaclyn2.jpg") },
       { title: "Headshots", path: headshots_index_path, image_url: view_context.asset_path("/assets/Anna.jpg") },
       { title: "Couples", path: couples_index_path, image_url: view_context.asset_path("/assets/treandanna2.jpg") },
       { title: "Boudoir", path: boudoir_index_path, image_url: view_context.asset_path("/assets/Bre.jpg") },
